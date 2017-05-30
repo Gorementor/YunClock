96f9f7b0e25c620ec9fe41289008f7ecf5d67f20
Sven <Gorementor@users.noreply.github.com>
2017-05-31 00:39:21 +0200

    checkin

--- __vm/.YunClock.vsarduino.h
+++ __vm/.YunClock.vsarduino.h
@@ -1,57 +1,91 @@
 /* 
 	Editor: http://www.visualmicro.com
-	        visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
-	        the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
-	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
-	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
+			visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
+			the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
+			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
+			note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: Arduino Yún, Platform=avr, Package=arduino
+	Hardware: Intel® Edison, Platform=i686, Package=Intel
 */
+
+#if defined(_VMICRO_INTELLISENSE)
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define __AVR_ATmega32u4__
-#define __AVR_ATmega32U4__
+#define __BIN_I686__
 #define _VMDEBUG 1
-#define F_CPU 16000000L
-#define ARDUINO 106013
-#define ARDUINO_AVR_YUN
-#define ARDUINO_ARCH_AVR
-#define USB_VID 0x2341
-#define USB_PID 0x8041
+#define __ARDUINO_X86__
+#define ARDUINO 10802
 #define __cplusplus 201103L
-#define __AVR__
 #define __inline__
 #define __asm__(x)
 #define __extension__
+#define __ATTR_PURE__
+#define __ATTR_CONST__
 #define __inline__
 #define __volatile__
-#define GCC_VERSION 40801
-#define volatile(va_arg) 
-#define _CONST
-#define __builtin_va_start
-#define __builtin_va_end
-#define __attribute__(x)
-#define NOINLINE __attribute__((noinline))
+
+
+#define __ICCARM__
+#define __ASM
+#define __INLINE
+#define __builtin_va_list void
+//#define _GNU_SOURCE 
+//#define __GNUC__ 0
+//#undef  __ICCARM__
+//#define __GNU__
+
+typedef long Pio;
+typedef long Efc;
+typedef long Adc;
+typedef long Pwm;
+typedef long Rtc;
+typedef long Rtt;
+typedef long pRtc;
+typedef long Spi;
+typedef long spi;
+typedef long Ssc;
+//typedef long p_scc;
+typedef long Tc;
+//typedef long pTc;
+typedef long Twi;
+typedef long Wdt;
+//typedef long pTwi;
+typedef long Usart;
+typedef long Pdc;
+typedef long Rstc;
+
+extern const int ADC_MR_TRGEN_DIS = 0;
+extern const int ADC_MR_TRGSEL_ADC_TRIG0 = 0;
+extern const int ADC_MR_TRGSEL_Pos = 0;
+
+extern const int ADC_MR_TRGSEL_Msk = 0;
+extern const int ADC_MR_TRGEN = 0;
+extern const int ADC_TRIG_TIO_CH_0 = 0;
+extern const int ADC_MR_TRGSEL_ADC_TRIG1 = 0;
+extern const int ADC_TRIG_TIO_CH_1 = 0;
+extern const int ADC_MR_TRGSEL_ADC_TRIG2 = 0;
+extern const int ADC_MR_TRGSEL_ADC_TRIG3 = 0;
+
+#define __ARMCC_VERSION 400678
+#define __attribute__(noinline)
+
 #define prog_void
 #define PGM_VOID_P int
-#ifndef __builtin_constant_p
-	#define __builtin_constant_p __attribute__((__const__))
-#endif
-#ifndef __builtin_strlen
-	#define __builtin_strlen  __attribute__((__const__))
-#endif
-#define NEW_H
-typedef void *__builtin_va_list;
+
+
+            
+typedef unsigned char byte;
 extern "C" void __cxa_pure_virtual() {;}
 
 
 
 #include <arduino.h>
 #include <pins_arduino.h> 
-#undef F
-#define F(string_literal) ((const PROGMEM char *)(string_literal))
-#undef PSTR
-#define PSTR(string_literal) ((const PROGMEM char *)(string_literal))
+#include <variant.h> 
+#include <variant.cpp> 
+#undef cli
+#define cli()
 #include "YunClock.ino"
+#endif
 #endif
